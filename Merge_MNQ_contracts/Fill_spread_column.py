7e7a7dd8712edc0ec09b533c1e160739a55fe206
Vova deduskin lap <vladimir.sehirev@gmail.com>
2025-11-08 21:42:20 +0200

    before Tallin

--- Merge_MNQ_contracts/Fill_spread_column.py
+++ Merge_MNQ_contracts/Fill_spread_column.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
 # Input and output2 file paths
-input_file = "merged_files_outputs/MNQ_merged.csv"
-output_file = "merged_files_outputs/MNQ_merged_spread.csv"
+input_file = "MNQ_merged.csv"
+output_file = "MNQ_merged_no_spread.csv"
 
 # Read CSV (tab-delimited)
 print("Reading file:", input_file)
