104260df3e81758c50aab21fd35bda42432c8b03
Soho36 <vladimir.sehirev@gmail.com>
2025-12-01 23:03:18 +0200

    before pull from silla

--- XLSX_files_split/XLSX_file_split.py
+++ XLSX_files_split/XLSX_file_split.py
@@ -3,7 +3,7 @@ import math
 import os
 
 # Input file
-input_file = "../XLSX_files_split/input/time_shifted_greengreen.xlsx"
+input_file = "../XLSX_files_split/input/time_shifted_99.xlsx"
 file_name = os.path.basename(input_file)  # Extract file name from input_file
 # Output base name
 output_base = f"trades_part_{file_name}"
@@ -18,7 +18,7 @@ print("Reading file:", input_file)
 # Calculate how many parts we need
 num_parts = math.ceil(len(df) / rows_per_file)
 print("Calculating number of parts:", num_parts)
-folder_name = "output"
+folder_name = "output99"
 
 os.makedirs(folder_name, exist_ok=True)
 # Split into chunks and save
